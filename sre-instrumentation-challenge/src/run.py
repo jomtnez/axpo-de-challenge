d60a6dc1b6d22e936b1557dc702fda710df93ef7
jomtnez <mtnezlo26@gmail.com>
2025-02-25 17:58:30 +0100

    Remove other challenges

--- sre-instrumentation-challenge/src/run.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from storage import app
-from waitress import serve
-
-if __name__ == "__main__":
-    port = os.getenv("PORT", default=5000)
-    serve(app, host="0.0.0.0", port=port)
--- sre-instrumentation-challenge/src/storage/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-
-from storage.bucket import bucket_blueprint
-
-app = Flask(__name__, static_url_path="", static_folder="../static")
-app.config.from_object(__name__)
-app.register_blueprint(bucket_blueprint, url_prefix="/api")
